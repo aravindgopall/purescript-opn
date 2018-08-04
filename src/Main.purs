1ae919f33d7407ac3a62711125f260651504a9b3
aravind.mallapureddy <aravind.mallapureddy@juspay.in>
2018-08-04 08:10:23 +0530

    basic adds

--- src/Main.purs
+++ src/Main.purs
@@ -7,10 +7,7 @@ import Control.Monad.Eff.Console (CONSOLE, log, logShow)
 import Data.Maybe (Maybe(..))
 import Node.ChildProcess (CHILD_PROCESS, defaultExecOptions, exec)
 
-foreign import open ::forall eff. String -> Eff eff Unit
 foreign import process :: forall vals. { | vals}
-
-
 
 main :: forall e. Eff (console :: CONSOLE, cp :: CHILD_PROCESS | e) Unit
 main = do
@@ -19,19 +16,10 @@ main = do
 
 
 openBrowser :: forall eff. String -> Maybe String -> Eff (cp :: CHILD_PROCESS, console :: CONSOLE | eff) Unit
-openBrowser str mArgs = exec (getArgsWithCmd str mArgs)  defaultExecOptions (\resp -> logShow resp.stderr *> logShow resp.stdout)
+openBrowser str mArgs = exec (getCmd str mArgs)  defaultExecOptions (\resp -> logShow resp.stderr *> logShow resp.stdout)
 
-
-getArgsWithCmd :: String -> Maybe String -> String
-getArgsWithCmd str mArgs = 
-  let cmd = getCmd
-      in case mArgs of 
-           Just val -> cmd <> str <>" --args" <> val
-           Nothing -> cmd <> str
-
-getCmd :: String
-getCmd = case process.platform of 
-          "darwin" -> "open "
-          "android" -> "xdg-open "
-
-          _ -> "cmd "
+getCmd :: String -> Maybe String ->String
+getCmd val mArgs= case process.platform of 
+          "darwin" -> "open " <> val <> " --args --" <> (show mArgs)
+          "android" -> "xdg-open " <> val 
+          _ -> "cmd start " <> val <> (show mArgs)  
